e68931efa4392dc984cb46631e307ab9715bec25
itsluo <itsluo@foxmail.com>
2021-08-30 23:11:11 +0800

    add config

--- install.sh
+++ install.sh
@@ -2,42 +2,43 @@
 
 echo "install begin"
 
-tool_set=(python3 python3-pip make gcc zsh tmux cscope ctags zplug fzf)
+tool_set=(python3 python3-pip make gcc zsh tmux cscope ctags zplug fzf thefuck autojump)
 
 PKG_TOOL="apt-get"
 
-# sudo ${PKG_TOOL} update
+sudo ${PKG_TOOL} update
 
-# for element in ${tool_set[@]}
-# do
-#     cmd="sudo ${PKG_TOOL} install ${element}"
-#     echo ""
-#     echo ">>>   ${cmd}"
-#     $cmd
-# done
+for element in ${tool_set[@]}
+do
+    cmd="sudo ${PKG_TOOL} install ${element}"
+    echo ""
+    echo ">>>   ${cmd}"
+    $cmd
+done
 
 
 # 通过git下载源码
-git_set=(ryanoasis/nerd-fonts ohmyzsh/ohmyzsh tmux-plugins/tpm)
+#ryanoasis/nerd-fonts 
+git_set=(ohmyzsh/ohmyzsh tmux-plugins/tpm)
 git_src_dir=$HOME/tmp
 
 if [ ! -d $git_src_dir ]; then
     mkdir $git_src_dir 
 fi
 
-# cd $git_src_dir 
-
-# for element in ${git_set[@]}
-# do
-#     cmd="git clone git@github.com:${element}.git"
-#     echo ""
-#     echo ">>>   ${cmd}"
-#     $cmd
-# done
+cd $git_src_dir 
+for element in ${git_set[@]}
+do
+    array=(${element//// })
+    if [ ! -d ${array[1]} ]; then
+        cmd="git clone git@github.com:${element}.git"
+        echo ""
+        echo ">>>   ${cmd}"
+        $cmd
+    fi
+done
 
 #源码安装
-
-
 
 if [ -d $git_src_dir/nerd-fonts ]; then
     if [ ! -d $HOME/.local/share/fonts/NerdFonts ];  then
@@ -65,3 +66,29 @@ sudo pip install mkdocs
 
 echo ""
 echo "install end"
+
+
+echo ""
+echo "init config begin"
+
+EXEC_DIR=$(cd `dirname $0`; pwd)
+#vim
+if [ ! -d ~/.vim ]; then
+    cp ${EXEC_DIR}/vim-conf/.vimrc ~ 
+    cp ${EXEC_DIR}/vim-conf/.vim ~ -r
+    source ~/.vimrc
+fi
+
+if [ ! -f ~/.tmux.conf ]; then
+    cp ${EXEC_DIR}/tmux-conf/. ~ 
+    source ~/.tmux.conf
+fi
+
+if [ ! -f ~/.zshrc ]; then
+    cp ${EXEC_DIR}/zsh-conf/.zshrc ~ 
+    source ~/.zshrc
+fi
+
+echo "init config end"
+
+
